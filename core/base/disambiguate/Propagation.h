92c2e72ac828146480f296923b524126463eaea8
Jonas Lukasczyk <jl@jluk.de>
2020-02-12 18:43:31 +0100

    added propagation rank and added parallel flood-fill-based region computation

--- core/base/disambiguate/Propagation.h
+++ core/base/disambiguate/Propagation.h
@@ -22,8 +22,8 @@ namespace ttk {
     // propagation data
     idType extremumIndex{-1};
     idType lastEncounteredSaddle{-1};
-    bool isTerminated{false};
     idType regionWriteIndex{0};
+    idType regionSize{0};
     std::vector<idType> region;
     boost::heap::fibonacci_heap< std::pair<idType,idType> > queue;
 
@@ -53,51 +53,52 @@ namespace ttk {
         uf0 = uf0->find();
         uf1 = uf1->find();
 
-        if(uf0 == uf1)
+        if(uf0 == uf1){
+            std::cout<<"xxxxxxxxxxxx"<<std::endl;
             return uf0;
+        }
 
         Propagation<idType>* master = nullptr;
         Propagation<idType>* slave  = nullptr;
 
         // determine master and slave based on rank
-        // if(uf0 == uf1) {
-        //     return uf0;
-        // } else if(uf0->rank > uf1->rank) {
-        //     master = uf0;
-        //     slave = uf1;
-        // } else if(uf0->rank < uf1->rank) {
-        //     master = uf1;
-        //     slave = uf0;
-        // } else {
-        //     master = uf0;
-        //     slave = uf1;
-        //     master->setRank(master->rank + 1);
-        // }
-
-        // determine master and slave based on region size
-        if(uf0->region.size() > uf1->region.size()) {
+        if(uf0 == uf1) {
+            return uf0;
+        } else if(uf0->rank > uf1->rank) {
             master = uf0;
             slave = uf1;
-        } else {
+        } else if(uf0->rank < uf1->rank) {
             master = uf1;
             slave = uf0;
+        } else {
+            master = uf0;
+            slave = uf1;
+            master->setRank(master->rank + 1);
         }
+
+        // determine master and slave based on region size
+        // if(uf0->region.size() > uf1->region.size()) {
+        //     master = uf0;
+        //     slave = uf1;
+        // } else {
+        //     master = uf1;
+        //     slave = uf0;
+        // }
 
         // update union find tree
         slave->setParent(master);
-
-        // mark slave as terminated
-        slave->isTerminated = true;
 
         // merge f. heaps
         master->queue.merge(slave->queue);
 
         // merge regions
-        idType oldSize = master->region.size();
-        idType newSize = oldSize + slave->region.size();
-        master->region.resize(newSize);
-        for(idType i=oldSize,j=0; i<newSize; i++,j++)
-            master->region[i] = slave->region[j];
+        master->regionSize += slave->regionSize;
+
+        // idType oldSize = master->region.size();
+        // idType newSize = oldSize + slave->region.size();
+        // master->region.resize(newSize);
+        // for(idType i=oldSize,j=0; i<newSize; i++,j++)
+        //     master->region[i] = slave->region[j];
 
         return master;
     }
