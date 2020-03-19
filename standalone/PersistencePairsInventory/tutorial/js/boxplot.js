adf8dbbcf670bae8e41b55e2d67ed6a044d4ccfe
Weihai Shen <wshen24@asu.edu>
2020-03-18 17:42:36 -0700

    updated

--- standalone/PersistencePairsInventory/tutorial/js/boxplot.js
+++ standalone/PersistencePairsInventory/tutorial/js/boxplot.js
@@ -33,9 +33,10 @@ function drawBoxplotCurve(numberofcomponents, data) {
         return ;
     }
     Window.box_plot_config = {
-        min_max_color: "#abdda4",
-        q1_q3_color: "#3288bd",
+        min_max_color: "#ef8a62",
+        q1_q3_color: "#fddbc7",
         background_color: "white",
+        median_line_color: "black",
         containerWidth: 561,
         containerHeight: 321,
         margin: {
@@ -439,7 +440,7 @@ function drawBoxplotCurve(numberofcomponents, data) {
             .attr("name", "box_polygon_line")
             .datum(points['median'])
             .attr("fill", "none")
-            .attr("stroke", "red")
+            .attr("stroke", Window.box_plot_config.median_line_color)
             .attr("stroke-width", 1)
             .attr("d", function (d) {
                 return Window.box_plot_config.line(xScale, yScale)(d);
