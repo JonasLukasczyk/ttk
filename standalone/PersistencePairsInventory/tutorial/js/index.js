06163b74e6ceee3bb7f9bf4cfcf82366851b1b22
Weihai Shen <wshen24@asu.edu>
2020-04-24 10:29:01 -0700

    updated

--- standalone/PersistencePairsInventory/tutorial/js/index.js
+++ standalone/PersistencePairsInventory/tutorial/js/index.js
@@ -9,11 +9,11 @@ Window.PPI = {
     // color range
     // "color-green": ["#edf8e9", "#bae4b3", "#74c476", "#31a354", "#006d2c"], 
     // "color-gray": ["#f7f7f7", "#d9d9d9", "#bdbdbd", "#969696", "#636363"],
-	// "color-red": ["#fee5d9", "#fcae91", "#fb6a4a", "#de2d26", "#a50f15"],  // red => blue
+	// "color-red": ["#fee5d9", "#fcae91", "#fb6a4a", "#de2d26", "#a50f15"],
 	// "color-blue": ["#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"],
-	"color-green": ["#edf8e9", "#bae4b3", "#74c476", "#31a354", "#006d2c"], 
+	"color-green": ["#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"], 
     "color-gray": ["#f7f7f7", "#d9d9d9", "#bdbdbd", "#969696", "#636363"],
-	"color-red": ["#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"],  // red => blue
+	"color-red": ["#fee5d9", "#fcae91", "#fb6a4a", "#de2d26", "#a50f15"],
 	"color-blue": ["#edf8e9", "#bae4b3", "#74c476", "#31a354", "#006d2c"],
 	// the number of elements for the width in the histogram
 	"histogram-width": 0,
