7b31d1068c72ac5158343b45bcca7565699d68c5
emmwel <emma.nilsson@liu.se>
2022-01-17 07:55:50 +0100

    cleaned code for random point generator

--- core/base/randomPointsGenerator/RandomPointsGenerator.h
+++ core/base/randomPointsGenerator/RandomPointsGenerator.h
@@ -3,8 +3,8 @@
 /// \author Emma Nilsson <emma.nilsson@liu.se>
 /// \date 2021-08-31.
 ///
-/// This module defines the %RandomPointsGenerator class that computes vector field
-/// based on Perlin Noise.
+/// This module defines the %RandomPointsGenerator class that generates random
+/// points in a given domain with scalar attributes.
 ///
 /// \b Related \b publication: \n
 
