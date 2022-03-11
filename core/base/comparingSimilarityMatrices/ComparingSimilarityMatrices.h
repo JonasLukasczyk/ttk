b2e00abc253ade3b4840735b0ca0f502018025d0
emmwel <emma.nilsson@liu.se>
2022-03-11 12:10:05 +0100

    new definition of continuation implemented in comparing matrices

--- core/base/comparingSimilarityMatrices/ComparingSimilarityMatrices.h
+++ core/base/comparingSimilarityMatrices/ComparingSimilarityMatrices.h
@@ -80,37 +80,78 @@ namespace ttk {
           ++curEvents.deaths[0];
         else if(nOnesAlg == 1)
           ++curEvents.continuations[0];
-        else if(nOnesAlg >= 2)
+        else if(nOnesAlg >= 2) {
           ++curEvents.splits[0];
+
+          // Even if the feature splits into two or more, it still continues
+          ++curEvents.continuations[0];
+        }
 
         if(nOnesGT == 0)
           ++curEvents.deaths[1];
         else if(nOnesGT == 1)
           ++curEvents.continuations[1];
-        else if(nOnesGT >= 2)
+        else if(nOnesGT >= 2) {
           ++curEvents.splits[1];
-
+          // Even if the feature splits into two or more, it still continues
+          ++curEvents.continuations[1];
+        }
         // Check correctness
+        if(nOnesAlg == nOnesGT && nOnesAlg == 0) {
+          ++curEvents.deaths[2];
+        }
+
         if(nOnesAlg == nOnesGT && nOnesAlg == 1
            && columnFillAlg[indexIdMapAlg0[c]][0]
                 == columnFillGT[indexIdMapGT0[c]][0])
           ++curEvents.continuations[2];
 
-        if(nOnesAlg == nOnesGT && nOnesAlg == 0) {
-          ++curEvents.deaths[2];
-        }
-
-        // To check correctness, check that the feature ids involved in the
-        // split matches
+        // To check correctness of the split: check that the feature ids
+        // involved in the split matches
         if(nOnesAlg == nOnesGT && nOnesAlg >= 2) {
-          bool match = true;
+          bool completeMatch = true;
+          bool atLeastOneMatch = false;
           for(int i = 0; i < nOnesAlg; i++) {
             if(columnFillAlg[indexIdMapAlg0[c]][i]
                != columnFillGT[indexIdMapGT0[c]][i])
-              match = false;
+              completeMatch = false;
+            else
+              atLeastOneMatch = true;
           }
-          if(match)
+          if(completeMatch)
             ++curEvents.splits[2];
+          if(atLeastOneMatch)
+            ++curEvents.continuations[2];
+        }
+
+        // Check if a continuation occurs within the split, even though it
+        // initself is not correct
+        if(nOnesAlg >= 2 && nOnesGT < nOnesAlg) {
+          bool atLeastOneMatch = false;
+          for(int i = 0; i < nOnesAlg; i++) {
+            for(int j = 0; j < nOnesGT; j++) {
+              if(columnFillAlg[indexIdMapAlg0[c]][i]
+                 == columnFillGT[indexIdMapGT0[c]][j]) {
+                atLeastOneMatch = true;
+              }
+            }
+          }
+          if(atLeastOneMatch)
+            ++curEvents.continuations[2];
+        }
+
+        if(nOnesGT >= 2 && nOnesAlg < nOnesGT) {
+          bool atLeastOneMatch = false;
+          for(int i = 0; i < nOnesGT; i++) {
+            for(int j = 0; j < nOnesAlg; j++) {
+              if(columnFillAlg[indexIdMapAlg0[c]][j]
+                 == columnFillGT[indexIdMapGT0[c]][i]) {
+                atLeastOneMatch = true;
+              }
+            }
+          }
+          if(atLeastOneMatch)
+            ++curEvents.continuations[2];
         }
       }
 
@@ -125,16 +166,12 @@ namespace ttk {
           ++curEvents.births[0];
         else if(nOnesAlg >= 2) {
           ++curEvents.merges[0];
-          // If features merge they are no longer continuations
-          curEvents.continuations[0] -= nOnesAlg;
         }
 
         if(nOnesGT == 0)
           ++curEvents.births[1];
         else if(nOnesGT >= 2) {
           ++curEvents.merges[1];
-          // If features merge they are no longer continuations
-          curEvents.continuations[1] -= nOnesGT;
         }
 
         // Check correctness
@@ -151,8 +188,9 @@ namespace ttk {
                != rowFillGT[indexIdMapGT1[r]][i])
               match = false;
           }
-          if(match)
+          if(match) {
             ++curEvents.merges[2];
+          }
         }
       }
 
