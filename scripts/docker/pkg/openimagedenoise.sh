91699c0c4082f59b67ea805ea18b79ca29094c7f
m-s-will <mswill@rptu.de>
2025-04-04 10:10:37 +0200

    cmake fix for openimagedenoise

--- scripts/docker/pkg/openimagedenoise.sh
+++ scripts/docker/pkg/openimagedenoise.sh
@@ -9,6 +9,7 @@ if [ $(arch) == "x86_64" ]; then
     fetch-src https://github.com/OpenImageDenoise/oidn/releases/download/v${OIDN_VERSION}/oidn-${OIDN_VERSION}.src.tar.gz
 
     cmake-default \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
         -DOIDN_APPS=OFF
 
 fi
